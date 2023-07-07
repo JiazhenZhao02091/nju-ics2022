bed6be15183d6edd6ed27674e7b3218ab1a008ce
JiazhenZhao <873694994@qq.com>
2023-07-07 12:04:06 +0800

    implenments the op

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -116,11 +116,11 @@ static bool make_token(char *e) {
 	/* Try all rules one by one. */
 	for (i = 0; i < NR_REGEX; i ++) {
 	    if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-		char *substr_start = e + position;
+	//	char *substr_start = e + position;
 		int substr_len = pmatch.rm_eo;
 
-		Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-			i, rules[i].regex, position, substr_len, substr_len, substr_start);
+		//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+		//	i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
 		position += substr_len;
 
@@ -218,13 +218,15 @@ static bool make_token(char *e) {
 
 bool check_parentheses(int p, int q)
 {
-    if(tokens[p].type != ZUO  || tokens[q].type != YOU)
+   // return true;
+//    printf("p = %d, q = %d\n",tokens[p].type, tokens[q].type);
+    if(tokens[p].type != '('  || tokens[q].type != ')')
 	return false;
     int l = p , r = q;
     while(l < r)
     {
-	if(tokens[l].type == ZUO){
-	    if(tokens[r].type == YOU)
+	if(tokens[l].type == '('){
+	    if(tokens[r].type == ')')
 	    {
 		l ++ , r --;
 		continue;
@@ -233,7 +235,7 @@ bool check_parentheses(int p, int q)
 	    else 
 		r --;
 	}
-	else if(tokens[l].type == YOU)
+	else if(tokens[l].type == ')')
 	    return false;
 	else l ++;
     }
@@ -263,13 +265,24 @@ uint32_t eval(int p, int q) {
 	/* The expression is surrounded by a matched pair of parentheses.
 	 * If that is the case, just throw away the parentheses.
 	 */
+	// printf("check p = %d, q = %d\n",p + 1 , q - 1);
 	return eval(p + 1, q - 1);
     }
+   /* else if(check_parentheses(p, q) == false){
+    	printf("Unique\n");
+	return -1;
+    }
+    */
     else {
 	int op = -1; // op = the position of 主运算符 in the token expression;
 	bool flag = false; 
 	for(int i = p ; i <= q ; i ++)
 	{
+	    if(tokens[i].type == '(')
+	    {
+		while(tokens[i].type != ')')
+			i ++;
+	    }
 	    if(tokens[i].type == '+' || tokens[i].type == '-'){
 		flag = true;
 		op = max(op, i);
@@ -278,13 +291,14 @@ uint32_t eval(int p, int q) {
 		op = max(op, i);
 	    }
 	}
+//	printf("op position is %d\n", op);
 	// if register return $register
 	int  op_type = tokens[op].type;
 
 	// 递归处理剩余的部分
 	uint32_t  val1 = eval(p, op - 1);
 	uint32_t  val2 = eval(op + 1, q);
-	printf("val1 = %d, val2 = %d \n", val1, val2);
+//	printf("val1 = %d, val2 = %d \n", val1, val2);
 
 	switch (op_type) {
 	    case '+': 
@@ -315,7 +329,7 @@ word_t expr(char *e, bool *success)
     /* TODO: Insert codes to evaluate the expression. */
     *success = true;// Return token zhuang tai
 
-    for(int i = 0 ; i < len ; i ++){printf("token[%d] type = [%d]\n", i, tokens[i].type);}
+    // for(int i = 0 ; i < len ; i ++){printf("token[%d] type = [%d]\n", i, tokens[i].type);}
     /*
      *  Get length
      */ 
@@ -326,14 +340,14 @@ word_t expr(char *e, bool *success)
 	    break;
 	tokens_len ++;
     }
-	printf("tokens_len = %d \n" , tokens_len);
     /*
      * True Expr
      * Data tokens (num, op_type)
      */ 
     uint32_t res = 0;
-    printf("Begin calc <===================>\n");
+//    printf("Begin calc <===================>\n");
     res = eval(0, tokens_len - 1);
+ //   printf("check flag = %d\n",check_parentheses(0, tokens_len - 1));
     printf("uint32_t res = %d\n", res);		
 
     return 0;
--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,17 +1,17 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <isa.h>
 #include <cpu/cpu.h>
@@ -26,172 +26,173 @@ void init_wp_pool();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
-  static char *line_read = NULL;
+    static char *line_read = NULL;
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
+    if (line_read) {
+	free(line_read);
+	line_read = NULL;
+    }
 
-  line_read = readline("(nemu) ");
+    line_read = readline("(nemu) ");
 
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
+    if (line_read && *line_read) {
+	add_history(line_read);
+    }
 
-  return line_read;
+    return line_read;
 }
 
 static int cmd_c(char *args) {
-  cpu_exec(-1);
-  return 0;
+    cpu_exec(-1);
+    return 0;
 }
 // CMD_INFO get the reg info
 static int cmd_info(char *args){
-	if(strcmp(args, "r") == 0)
-		isa_reg_display();
-	return 0;
+    if(strcmp(args, "r") == 0)
+	isa_reg_display();
+    return 0;
 }
 
 // CMD_Q quit the NEMU
 static int cmd_q(char *args) {
-  nemu_state.state = NEMU_QUIT;
-  return -1;
+    nemu_state.state = NEMU_QUIT;
+    return -1;
 }
 
 // CMD_X scan virtual memory
 static int cmd_x(char *args){
-	char* n = strtok(args," ");
-	char* baseaddr = strtok(NULL," ");
-	int len = 0;
-	paddr_t addr = 0;
-	sscanf(n, "%d", &len);
-	sscanf(baseaddr,"%x", &addr);
-       for(int i = 0 ; i < len ; i ++)
-       {
-	        printf("%x\n",paddr_read(addr,4));//addr len
-		addr = addr + 4;
-       }	       
-	return 0;
+    char* n = strtok(args," ");
+    char* baseaddr = strtok(NULL," ");
+    int len = 0;
+    paddr_t addr = 0;
+    sscanf(n, "%d", &len);
+    sscanf(baseaddr,"%x", &addr);
+    for(int i = 0 ; i < len ; i ++)
+    {
+	printf("%x\n",paddr_read(addr,4));//addr len
+	addr = addr + 4;
+    }	       
+    return 0;
 }
 
 // expr + - * /
 static int cmd_expr(char* args){
-  if(args == NULL){
-      printf("No args\n");
-      return 0;
-  }
-  printf("args = %s\n", args);
-  bool flag = false;
-  expr(args, &flag);
-  if(flag)
-  printf("Success token\n");
-  else 
-  printf("Unsuccessful\n");
-  return 0;
+    if(args == NULL){
+	printf("No args\n");
+	return 0;
+    }
+    //  printf("args = %s\n", args);
+    bool flag = false;
+    expr(args, &flag);
+    /* if(flag)
+       printf("Success token\n");
+       else 
+       printf("Unsuccessful\n");
+       */
+    return 0;
 }
 
 
 
 
 static int cmd_si(char *args){
-	int step = 0;
-	if(args == NULL)
- 		step = 1;
-	else
-		sscanf(args,"%d",&step);// 读入 Step
-	cpu_exec(step);
-	return 0;
+    int step = 0;
+    if(args == NULL)
+	step = 1;
+    else
+	sscanf(args,"%d",&step);// 读入 Step
+    cpu_exec(step);
+    return 0;
 }
 static int cmd_help(char *args);
 
 static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
+    const char *name;
+    const char *description;
+    int (*handler) (char *);
 } cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "run si program", cmd_si },
-  { "info", "Get register info", cmd_info },
-  { "x", "Scan the virtual memory", cmd_x },
-  {"expr","run expr",cmd_expr},
-  /* TODO: Add more commands */
+    { "help", "Display information about all supported commands", cmd_help },
+    { "c", "Continue the execution of the program", cmd_c },
+    { "q", "Exit NEMU", cmd_q },
+    { "si", "run si program", cmd_si },
+    { "info", "Get register info", cmd_info },
+    { "x", "Scan the virtual memory", cmd_x },
+    {"expr","run expr",cmd_expr},
+    /* TODO: Add more commands */
 
 };
 
 #define NR_CMD ARRLEN(cmd_table)
 
 static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
+    /* extract the first argument */
+    char *arg = strtok(NULL, " ");
+    int i;
 
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    if (arg == NULL) {
+	/* no argument given */
+	for (i = 0; i < NR_CMD; i ++) {
+	    printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+	}
     }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
+    else {
+	for (i = 0; i < NR_CMD; i ++) {
+	    if (strcmp(arg, cmd_table[i].name) == 0) {
+		printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		return 0;
+	    }
+	}
+	printf("Unknown command '%s'\n", arg);
     }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
+    return 0;
 }
 
 void sdb_set_batch_mode() {
-  is_batch_mode = true;
+    is_batch_mode = true;
 }
 
 void sdb_mainloop() {
-  if (is_batch_mode) {
-    cmd_c(NULL);
-    return;
-  }
-
-  for (char *str; (str = rl_gets()) != NULL; ) {
-    char *str_end = str + strlen(str);
-
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
-
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end) {
-      args = NULL;
+    if (is_batch_mode) {
+	cmd_c(NULL);
+	return;
     }
+
+    for (char *str; (str = rl_gets()) != NULL; ) {
+	char *str_end = str + strlen(str);
+
+	/* extract the first token as the command */
+	char *cmd = strtok(str, " ");
+	if (cmd == NULL) { continue; }
+
+	/* treat the remaining string as the arguments,
+	 * which may need further parsing
+	 */
+	char *args = cmd + strlen(cmd) + 1;
+	if (args >= str_end) {
+	    args = NULL;
+	}
 
 #ifdef CONFIG_DEVICE
-    extern void sdl_clear_event_queue();
-    sdl_clear_event_queue();
+	extern void sdl_clear_event_queue();
+	sdl_clear_event_queue();
 #endif
 
-    int i;
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
-        break;
-      }
-    }
+	int i;
+	for (i = 0; i < NR_CMD; i ++) {
+	    if (strcmp(cmd, cmd_table[i].name) == 0) {
+		if (cmd_table[i].handler(args) < 0) { return; }
+		break;
+	    }
+	}
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
-  }
+	if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    }
 }
 
 void init_sdb() {
-  /* Compile the regular expressions. */
-  init_regex();
+    /* Compile the regular expressions. */
+    init_regex();
 
-  /* Initialize the watchpoint pool. */
-  init_wp_pool();
+    /* Initialize the watchpoint pool. */
+    init_wp_pool();
 }
